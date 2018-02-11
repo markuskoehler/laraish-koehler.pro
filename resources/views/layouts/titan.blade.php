dd26dfaf59303b2add8f48beefa71eb8fa59e3a1
Markus Köhler <info@koehler.pro>
2018-02-11 21:00:37 +0100

    titan blade reference proper assets path

--- resources/views/layouts/titan.blade.php
+++ resources/views/layouts/titan.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en-US" dir="ltr">
+<html lang="en-US" dir="ltr" prefix="og: http://ogp.me/ns#">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=10, user-scalable=no, initial-scale=1.0">
     <!--
     Document Title
     =============================================
@@ -29,6 +29,8 @@
     <link rel="manifest" href="/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="assets/images/favicons/ms-icon-144x144.png">
+    <link rel="shortcut icon" href="{{ public_url('images/favicon.ico') }}" type="image/vnd.microsoft.icon"/>
+    <link rel="apple-touch-icon-precomposed" href="{{ public_url('images/apple-touch-icon-precomposed.png') }}">
     <meta name="theme-color" content="#ffffff">
     <!--
     Stylesheets
@@ -36,24 +38,36 @@
 
     -->
     <!-- Default stylesheets-->
-    <link href="assets/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{public_url('/assets/lib/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Template specific stylesheets-->
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Volkhov:400i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
-    <link href="assets/lib/animate.css/animate.css" rel="stylesheet">
-    <link href="assets/lib/components-font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="assets/lib/et-line-font/et-line-font.css" rel="stylesheet">
-    <link href="assets/lib/flexslider/flexslider.css" rel="stylesheet">
-    <link href="assets/lib/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="assets/lib/owl.carousel/dist/assets/owl.theme.default.min.css" rel="stylesheet">
-    <link href="assets/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
-    <link href="assets/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
+    <link href="{{public_url('assets/lib/animate.css/animate.css')}}" rel="stylesheet">
+    <link href="{{public_url('assets/lib/components-font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{public_url('assets/lib/et-line-font/et-line-font.css')}}" rel="stylesheet">
+    <link href="{{public_url('assets/lib/flexslider/flexslider.css')}}" rel="stylesheet">
+    <link href="{{public_url('assets/lib/owl.carousel/dist/assets/owl.carousel.min.css')}}" rel="stylesheet">
+    <link href="{{public_url('assets/lib/owl.carousel/dist/assets/owl.theme.default.min.css')}}" rel="stylesheet">
+    <link href="{{public_url('assets/lib/magnific-popup/dist/magnific-popup.css')}}" rel="stylesheet">
+    <link href="{{public_url('assets/lib/simple-text-rotator/simpletextrotator.css')}}" rel="stylesheet">
     <!-- Main stylesheet and color file-->
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link id="color-scheme" href="assets/css/colors/default.css" rel="stylesheet">
+    <link href="{{public_url('assets/css/style.css')}}" rel="stylesheet">
+    <link id="color-scheme" href="{{public_url('assets/css/colors/default.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ public_url('css/app.css') }}"/>
+    <link rel="stylesheet" href="{{ get_stylesheet_uri() }}">
+
+    @yield('head')
+	<?php wp_head(); ?>
 </head>
 <body data-spy="scroll" data-target=".onpage-navigation" data-offset="60">
+
+@if(env('APP_ENV')==='production')
+    <script>
+        // scripts that only should be ran on production server.
+    </script>
+@endif
+
 <main>
     <div class="page-loader">
         <div class="loader">Loading...</div>
@@ -61,7 +75,8 @@
     <nav class="navbar navbar-custom navbar-fixed-top navbar-transparent" role="navigation">
         <div class="container">
             <div class="navbar-header">
-                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html">Titan</a>
+                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                <span class="navbar-brand"><a href="{{public_url()}}"><img src="{{public_url('images/mkit-dots.fw.png')}}" alt="Markus Koehler IT Services"></a><span class="slogan">&nbsp;All your web needs.</span></span>
             </div>
             <div class="collapse navbar-collapse" id="custom-collapse">
                 <ul class="nav navbar-nav navbar-right">
@@ -355,7 +370,7 @@
             </div>
         </div>
     </nav>
-    <section class="home-section home-parallax home-fade home-full-height bg-dark-60 agency-page-header" id="home" data-background="assets/images/agency/agency_bg.jpg">
+    <section class="home-section home-parallax home-fade home-full-height bg-dark-60 agency-page-header" id="home" data-background="{{public_url('assets/images/agency/agency_bg.jpg')}}">
         <div class="titan-caption">
             <div class="caption-content">
                 <div class="font-alt mb-30 titan-title-size-1">Grow your awesome idea</div>
@@ -826,18 +841,22 @@
 JavaScripts
 =============================================
 -->
-<script src="assets/lib/jquery/dist/jquery.js"></script>
-<script src="assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="assets/lib/wow/dist/wow.js"></script>
-<script src="assets/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js"></script>
-<script src="assets/lib/isotope/dist/isotope.pkgd.js"></script>
-<script src="assets/lib/imagesloaded/imagesloaded.pkgd.js"></script>
-<script src="assets/lib/flexslider/jquery.flexslider.js"></script>
-<script src="assets/lib/owl.carousel/dist/owl.carousel.min.js"></script>
-<script src="assets/lib/smoothscroll.js"></script>
-<script src="assets/lib/magnific-popup/dist/jquery.magnific-popup.js"></script>
-<script src="assets/lib/simple-text-rotator/jquery.simple-text-rotator.min.js"></script>
-<script src="assets/js/plugins.js"></script>
-<script src="assets/js/main.js"></script>
+<script src="{{public_url('assets/lib/jquery/dist/jquery.js')}}"></script>
+<script src="{{public_url('assets/lib/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script src="{{public_url('assets/lib/wow/dist/wow.js')}}"></script>
+<script src="{{public_url('assets/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js')}}"></script>
+<script src="{{public_url('assets/lib/isotope/dist/isotope.pkgd.js')}}"></script>
+<script src="{{public_url('assets/lib/imagesloaded/imagesloaded.pkgd.js')}}"></script>
+<script src="{{public_url('assets/lib/flexslider/jquery.flexslider.js')}}"></script>
+<script src="{{public_url('assets/lib/owl.carousel/dist/owl.carousel.min.js')}}"></script>
+<script src="{{public_url('assets/lib/smoothscroll.js')}}"></script>
+<script src="{{public_url('assets/lib/magnific-popup/dist/jquery.magnific-popup.js')}}"></script>
+<script src="{{public_url('assets/lib/simple-text-rotator/jquery.simple-text-rotator.min.js')}}"></script>
+<script src="{{public_url('assets/js/plugins.js')}}"></script>
+<script src="{{public_url('assets/js/main.js')}}"></script>
+<script src="{{public_url('js/app.js')}}"></script>
+
+@yield('footer')
+<?php wp_footer();?>
 </body>
 </html>
