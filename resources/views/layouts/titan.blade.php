1170d30ffa595ad87cca2d4a0a3d5bc67d4a7244
Markus Köhler <info@koehler.pro>
2018-02-13 21:31:46 +0100

    raw titan blade with menu, to split up

--- resources/views/layouts/titan.blade.php
+++ resources/views/layouts/titan.blade.php
@@ -72,7 +72,144 @@
     <div class="page-loader">
         <div class="loader">Loading...</div>
     </div>
+    @php
+        add_action( 'init', 'register_my_menu' );
+
+    function register_my_menu() {
+        register_nav_menu( 'primary-menu', __( 'Primary Menu' ) );
+    }
+            class themeslug_walker_nav_menu extends Walker_Nav_Menu {
+
+        // add classes to ul sub-menus
+        function start_lvl( &$output, $depth = 0, $args = array() ) {
+            // depth dependent classes
+            $indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' ); // code indent
+            $display_depth = ( $depth + 1); // because it counts the first submenu as 0
+            $classes = array(
+                'sub-menu',
+                'dropdown-menu',
+                ( $display_depth % 2  ? 'menu-odd' : 'menu-even' ),
+                ( $display_depth >=2 ? 'sub-sub-menu' : '' ),
+                'menu-depth-' . $display_depth
+                );
+            $class_names = implode( ' ', $classes );
+
+            // build html
+            $output .= "\n" . $indent . '<ul class="' . $class_names . '">' . "\n";
+        }
+
+        // add main/sub classes to li's and links
+         function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+            global $wp_query;
+
+            $indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' ); // code indent
+
+            // depth dependent classes
+            $depth_classes = array(
+                ( $depth == 0 ? 'dropdown' : 'dropdown' ),
+                ( $depth >=2 ? 'sub-sub-menu-item' : '' ),
+                ( $depth % 2 ? 'menu-item-odd' : 'menu-item-even' ),
+                'menu-item-depth-' . $depth
+            );
+            $depth_class_names = esc_attr( implode( ' ', $depth_classes ) );
+
+            // passed classes
+            $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+            $class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
+
+            // build html
+            $output .= $indent . '<li id="nav-menu-item-'. $item->ID . '" class="' . $depth_class_names . ' ' . $class_names . '">';
+
+            // link attributes
+            $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+            $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+            $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+            if($args->walker->has_children) {
+                $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '#';
+                $attributes .= ' class="dropdown-toggle menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '" data-toggle="dropdown"';
+            } else {
+                // todo # for parent
+                $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+                // todo dropdown-toggle for parents
+                $attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+            }
+
+            $item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+                $args->before,
+                $attributes,
+                $args->link_before,
+                apply_filters( 'the_title', $item->title, $item->ID ),
+                $args->link_after,
+                $args->after
+            );
+
+            // build html
+            $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+        }
+
+        /*function menu_set_dropdown( $sorted_menu_items, $args ) {
+    $last_top = 0;
+    foreach ( $sorted_menu_items as $key => $obj ) {
+        // it is a top lv item?
+        if ( 0 == $obj->menu_item_parent ) {
+            // set the key of the parent
+            $last_top = $key;
+        } else {
+            $sorted_menu_items[$last_top]->classes['dropdown'] = 'dropdown';
+        }
+    }
+    return $sorted_menu_items;
+}
+        add_filter( 'wp_nav_menu_objects', 'menu_set_dropdown', 10, 2 );*/
+        }
+
+    /**
+ * Add a parent CSS class for nav menu items.
+ *
+ * @param array  $items The menu items, sorted by each menu item's menu order.
+ * @return array (maybe) modified parent CSS class.
+ */
+function wpdocs_add_menu_parent_class( $items ) {
+    $parents = array();
+
+    // Collect menu items with parents.
+    foreach ( $items as $item ) {
+        if ( $item->menu_item_parent && $item->menu_item_parent > 0 ) {
+            $parents[] = $item->menu_item_parent;
+        }
+    }
+
+    // Add class.
+    foreach ( $items as $item ) {
+        if ( in_array( $item->ID, $parents ) ) {
+            $item->classes[] = 'menu-parent-item';
+        }
+    }
+    return $items;
+}
+add_filter( 'wp_nav_menu_objects', 'wpdocs_add_menu_parent_class' );
+    @endphp
     <nav class="navbar navbar-custom navbar-fixed-top navbar-transparent" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html">Titan</a>
+            </div>
+            <div class="collapse navbar-collapse" id="custom-collapse">
+    {{
+    wp_nav_menu( array(
+		//'theme_location' => 'top',
+		//'menu_id'        => 'top-menu',
+		'menu_class'    => 'nav navbar-nav navbar-right',
+		'container'     => '',
+		//'container_id'     => 'custom-collapse',
+		//'container_class'     => 'navbar navbar-custom navbar-fixed-top navbar-transparent',
+		'walker' => new themeslug_walker_nav_menu()
+	) )
+    }}
+            </div>
+        </div>
+    </nav>
+    {{--<nav class="navbar navbar-custom navbar-fixed-top navbar-transparent" role="navigation">
         <div class="container">
             <div class="navbar-header">
                 <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
@@ -369,7 +506,7 @@
                 </ul>
             </div>
         </div>
-    </nav>
+    </nav>--}}
     <section class="home-section home-parallax home-fade home-full-height bg-dark-60 agency-page-header" id="home" data-background="{{public_url('assets/images/agency/agency_bg.jpg')}}">
         <div class="titan-caption">
             <div class="caption-content">
