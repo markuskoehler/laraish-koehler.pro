3306bec8af0f6e0ad9f0eeabe23e792c84d91f4f
Markus Köhler <info@koehler.pro>
2018-02-15 19:00:28 +0100

    404 page, cleaned up

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.titan')
 
 @section('content')
-    <div class="flex-center full-height">
-        <section class="text--center">
-            <h1 class="text--xl">404</h1>
-            <p>Sorry, the page you are looking for could not be found.</p>
-            <a href="{{home_url()}}" class="button">Back to Home</a>
-        </section>
-    </div>
+    <section class="home-section home-parallax home-fade home-full-height bg-dark bg-dark-30" id="home" data-background="assets/images/section-4.jpg">
+        <div class="titan-caption">
+            <div class="caption-content">
+                <div class="font-alt mb-30 titan-title-size-4">Error 404</div>
+                <div class="font-alt">The requested URL was not found on this server.<br/>That is all we know.
+                </div>
+                <div class="font-alt mt-30"><a class="btn btn-border-w btn-round" href="index.html">Back to home page</a></div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('footer')
+    <script>
+        // cleaning up page, todo make configurable
+        //$('nav').hide();
+        //$('.navbar-custom + .main').css('margin-top', '0');
+        $('.pre-footer').hide();
+        $('footer').hide();
+        $('div.main').replaceWith(function() {
+            return $('section#home', this);
+        });
+    </script>
 @endsection
